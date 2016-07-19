fb1ba8554893f5d43331a32ecbd0b2edf7385f38
Dongeg <821220992@qq.com>
2016-07-19 08:43:48 +0800

    debug

--- js/script.js
+++ js/script.js
@@ -35,4 +35,22 @@ $(function(){
 		var i=this.value;
 		$("#photo").attr('src','img/g'+i+'.jpg')
 	})
+	//下拉列表插件
+	$(".show").bind('click',function(){
+		$(".list-main").slideToggle("fast");
+	});
+	var listMain=$(".list-main").children();
+	for(var i=0;i<listMain.length;i++){
+		$(listMain[i]).bind('mouseover',function(){
+			for(var j=0;j<listMain.length;j++){
+				$(listMain[j]).css("background-color","#F5F5F5");
+			}
+			$(this).css("background-color","#008CE1");
+		})
+		$(listMain[i]).bind('click',function(){
+			$(".show").text($(this).text());
+			$(".show").attr("value",$(this).attr("value"));
+			$(".list-main").slideUp("fast");
+		});
+	}
 })
